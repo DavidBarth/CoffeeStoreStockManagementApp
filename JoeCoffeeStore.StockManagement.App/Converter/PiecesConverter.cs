54fb5683e11ca02e8a7d1cdd068f920e601b6536
David Barth <davidbarth80@gmail.com>
2017-04-19 16:33:28 +0100

    PiecesConverter

---
+++ JoeCoffeeStore.StockManagement.App/Converter/PiecesConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace JoeCoffeeStore.StockManagement.App.Converter
+{
+    class PiecesConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int amount = (int)value;
+            if (amount > 1)
+                return value + " pcs";
+            return value + " pc";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
