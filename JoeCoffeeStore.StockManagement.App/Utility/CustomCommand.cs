8ba0f2068487731160f110fa7ce7f4d2c2bf4107
David Barth <davidbarth80@gmail.com>
2017-04-24 17:35:43 +0100

    Implement CustomCommand

---
+++ JoeCoffeeStore.StockManagement.App/Utility/CustomCommand.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace JoeCoffeeStore.StockManagement.App.Utility
+{
+    class CustomCommand : ICommand
+    {
+        //to store an action
+        private Action<object> execute;
+        
+        //
+        private Predicate<object> canExecute;
+
+        //the action that is passed in here is the code/method I want to execute
+        public CustomCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute; 
+        }
+
+        //will be called to invoke something on view model
+        public void Execute(object parameter)
+        {
+            execute(parameter);
+        }
+
+        //method checks canExecute which is null then it's true otherwise call
+        //canExecute with a parameter that will be a Predicate passed in
+        //and return result of that
+        public bool CanExecute(object parameter)
+        {
+            bool b = canExecute == null ? true : canExecute(parameter);
+            return b;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+            }
+
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+    }
+}
