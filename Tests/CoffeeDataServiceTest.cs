49ba993a19dc9547316d07436802580b9bd09819
David Barth <davidbarth80@gmail.com>
2017-04-26 16:42:19 +0100

    CoffeeDataServiceTest

---
+++ Tests/CoffeeDataServiceTest.cs
@@ -0,0 +1,35 @@
+﻿using JoeCoffeeStore.StockManagement.App.Services;
+using JoeCoffeeStore.StockManagement.DAL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tests.Mocks;
+
+namespace Tests
+{
+    [TestClass]
+    public class CoffeeDataServiceTest
+    {
+        /// <summary>
+        /// Tests Service/CoffeeDataService 
+        /// </summary>
+        private ICoffeeRepository repository;
+
+        [TestInitialize]
+        public void Init()
+        {
+            repository = new MockRepository();
+        }
+
+        [TestMethod]
+        public void GetCoffeeDetailTest()
+        {
+            //Arrange
+            var service = new CoffeeDataService(repository);
+
+            //Act
+            var coffee = service.GetCoffeeDetail(1);
+
+            //Assert
+            Assert.IsNotNull(coffee);
+        }
+    }
+}
